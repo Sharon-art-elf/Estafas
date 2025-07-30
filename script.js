3e00f8b5bceb9a01bb852f8324f3a3e9b45ece08
Sharon-art-elf <mariadelrosarioguerreromonfil@gmail.com>
2025-07-29 21:12:31 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Gracias por tu mensaje. Pronto nos pondremos en contacto contigo.");
+});
